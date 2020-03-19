33e2b837d26045e3a57cfa1adbe1cdd25cf2db1a
Ansh Puvvada <43775333+crusader2000@users.noreply.github.com>
2020-03-19 10:20:36 +0530

    Update outer_loop.m

--- soda_paper/outer_loop.m
+++ soda_paper/outer_loop.m
@@ -3,10 +3,15 @@ function X=outer_loop(x,n,k)
     L=int64(log2(n));
     
     [set_I,set_h_sigma,set_o_sigma,set_Z,set_tau,set_G]=get_the_sets(x,L,n,k);
+    close all;
     [s,union_of_all_sets]=get_s(set_I,L);
-    I_dash=get_I_dash(union_of_all_sets,s,L);
+    I_dash=get_I_dash(union_of_all_sets,s,L)
+    disp(set_I)
     set_of_X_r_I_dash=get_x_r_I_dash(I_dash,set_h_sigma,set_o_sigma,set_Z,set_tau,set_G,L,n);
-    X=get_the_fourier_transform(set_of_X_r_I_dash,I_dash);
+%       disp(set_of_X_r_I_dash)
+     X=get_the_fourier_transform(set_of_X_r_I_dash,I_dash,n);
+     plot(X);
+    
 end
 
 % Step 1 run a location inner loop L times
@@ -28,6 +33,8 @@ function [set_I,set_h_sigma,set_o_sigma,set_Z,set_tau,set_G]=get_the_sets(x,L,n,
         set_tau=[set_tau;tau];
         set_G=[set_G;G];
     end
+    disp("Exiting the get_the_sets function");
+%     disp(set_h_sigma(1,:));
 end
 
 % Step 2 of outer loop
@@ -37,19 +44,21 @@ end
 function [s,union_of_all_sets]=get_s(I,L)
     union_of_all_sets=[];
     for i=1:L
-        I{i}=sort(I{i});
+%         disp(I{i})
         union_of_all_sets=union(union_of_all_sets,I{i});
     end
+    disp("Found the union of all sets");
+    
+    % Need to include zero too
     s=zeros(max(union_of_all_sets)+1,1);
-    for i=1:length(union_of_all_sets)
-        search_element=union_of_all_sets(i);
-        for j=1:L
-            index=binarySearch(I{j},length(I{j}),search_element);
-            if index ~=-1
-                s(search_element+1)=s(search_element+1)+1;
-            end
+    for r=1:L
+        I_r=I{r};
+        elements=length(I_r);
+        for j=1:elements
+                s(I_r(j)+1)=s(I_r(j)+1)+1;
         end
     end
+    disp("Found s");
 end
 
 % Step 3 of outer loop
@@ -61,45 +70,43 @@ function I_dash=get_I_dash(union_of_all_sets,s,L)
             I_dash=[I_dash;union_of_all_sets(i)];
         end
     end
+   disp("Found I_dash");
 end
 
 % Step 4 of outer loop
 % We run the estimation inner loop L times to get X_r_I_dash
 function set_of_X_r_I_dash=get_x_r_I_dash(I_dash,set_h_sigma,set_o_sigma,set_Z,set_tau,set_G,L,n)
-    set_of_X_r_I_dash=[]
+    set_of_X_r_I_dash=[];
     for i=1:L
-        X_estim=find_estimates(set_h_sigma(i),set_o_sigma(i),set_tau(i),set_G(i),I_dash,set_Z(i),n);
-        set_of_X_r_I_dash=[set_of_X_r_I_dash;X_estim];
+        X_estim=find_estimates(set_h_sigma(i,:),set_o_sigma(i,:),set_tau(i),set_G(i,:),I_dash,set_Z(i,:),n);
+%         disp(X_estim);
+        set_of_X_r_I_dash{i}=X_estim;
     end
+    disp("found set_of_X_r_I_dash");
+   
 end
-
 
 % Step 5 of outer loop
 % We need to find the median of real and imaginary parts
 
-function X=get_the_fourier_transform(set_of_X_r_I_dash,I_dash)
-    X=zeros(length(set_of_X_r_I_dash(1)),1)
+function X=get_the_fourier_transform(set_of_X_r_I_dash,I_dash,n)
+    X=zeros(n,1);
     for i=1:length(I_dash)
-        X_i_real=[]
-        X_i_imag=[]
-        for j=1:size(set_of_X_r_I_dash,1)
-            X_i_real=[X_i_real,real(set_of_X_r_I_dash(j))];
-            X_i_imag=[X_i_imag,imag(set_of_X_r_I_dash(j))];
-        end
-        X(i)=median(X_i_real)+j*median(X_i_imag)
+        X_i_real=set_of_X_r_I_dash(:,i);
+        X_i_imag=set_of_X_r_I_dash(:,i);
+        X(i)=median(X_i_real)+1i*median(X_i_imag);
     end
 end
 
 
-%Step 6 : The estimates in estimation loop
-
 function X_estim=find_estimates(h_sigma,o_sigma,tau,G,I,Z,n)
-    X_estim=zeros(1,n+1);
-    
+    X_estim=zeros(1,n);
+%     disp("in find_sdtimates")
+%     length(I)
     for j=1:length(I)
-        i=I(j)+1;
-        disp(i)
-        X_estim(i)=(Z(h_sigma(i))*exp((1i*2*pi*tau*(i-1))/n))/G(o_sigma(i));
+        k=I(j);
+        k=mod(k,n)+1;
+        X_estim(k)=(Z(h_sigma(k))*exp((1i*2*pi*tau*(k-1))/n))/G(o_sigma(k));
+%         disp(X_estim(k))
     end
-    
 end
